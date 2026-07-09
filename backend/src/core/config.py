e343941e2e85c4dfceb02302a48fcc7d2a9f5ac3
kirichenkovm <vmkirichenko1991@yandex.ru>
2026-07-09 16:35:31 +0300

    Add config.py

---
+++ backend/src/core/config.py
@@ -0,0 +1,30 @@
+from pydantic_settings import BaseSettings
+from pathlib import Path
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+    REDIS_URL: str = "redis://backend-redis:6379/0"
+
+    BASE_DIR: Path = Path(__file__).resolve().parent.parent
+    STORAGE_DIR: Path = BASE_DIR / "storage" / "files"
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
+    class Config:
+        env_file = ".env"
+        extra = "ignore"
+
+
+settings = Settings()
+settings.STORAGE_DIR.mkdir(parents=True, exist_ok=True)
